12d5d96e7611ee909b53e4af88eee044ab09a1ad
Ade Setiawan <adesbsp@gmail.com>
2019-11-07 14:13:15 +0700

    add relation

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -10,6 +10,7 @@ class Course(models.Model):
 
     #realation Many2One
     responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible", index=True)
+    session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
 
 class Session(models.Model):
     _name = 'openacademy.session'
