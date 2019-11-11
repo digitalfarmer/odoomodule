7aa476027e6e41c8d0076fbf00cc316b9f4c70cb
Ade Setiawan <adesbsp@gmail.com>
2019-11-11 10:29:36 +0700

    advace view adding calendar view

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from datetime import timedelta
 from odoo import models, fields, api, exceptions
 
 class Course(models.Model):
@@ -52,6 +53,7 @@ class Session(models.Model):
     attendee_ids= fields.Many2many('res.partner', string="Attendees")
 
     taken_seats= fields.Float(string="Taken Seats", compute='_taken_seats')
+    end_date = fields.Date(string="End Date", store=True, compute='_get_end_date', inverse='_set_end_date')
 
     @api.depends('seats','attendee_ids')
     def _taken_seats(self):
@@ -77,6 +79,26 @@ class Session(models.Model):
                     'message': "Increase seats or remove excess attendees",
                 }
             }
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        for r in self:
+            if not (r.start_date and r.duration):
+                r.end_date = r.start_date
+                continue
+
+            start= fields.Datetime.from_string(r.start_date)
+            duration= timedelta(days=r.duration, seconds=-1)
+            r.end_date= start + duration
+
+    def _set_end_date(self):
+        for r in self:
+            if not(r.start_date and r.end_date):
+                continue
+
+            start_date = fields.Datetime.from_string(r.start_date)
+            end_date = fields.Datetime.from_string(r.end_date)
+            r.duration=(end_date - start_date).days + 1
+
     @api.constrains('instructor_id', 'attendee_ids')
     def _check_instructor_not_in_attendee(self):
         for r in self:
