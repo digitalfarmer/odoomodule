4aece3484ef03d4d2e254bfcd9bcfbf9ab6e105a
Ade Setiawan <adesbsp@gmail.com>
2019-11-11 14:57:49 +0700

    adding security feature

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -26,11 +26,13 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        #'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
         'views/openacademy.xml',
         'views/partner.xml',
+        'security/security.xml',
+        # 'security/ir.model.access.csv',
     ],
     # only loaded in demonstration mode
     'demo': [
